eba7a50f2e305607fee648bdb8ef2ce10f1a0f2c
nosetotail-dev <71747675+nosetotail-dev@users.noreply.github.com>
2022-06-19 14:22:09 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\coordinator;
+use App\Http\Controllers\address;
+use App\Mail\WelcomeMail;
+use App\Mail\rejectMail;
+use Illuminate\Support\Facades\Mail;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +24,58 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
+ 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('staff/home', [HomeController::class, 'staffHome'])->name('staff.home')->middleware('user_type');
+Route::get('coordinator/home', [HomeController::class, 'coordinatorHome'])->name('coordinator.home')->middleware('user_type');
+Route::get('applicant/home', [HomeController::class, 'applicantHome'])->name('applicant.home')->middleware('user_type');
+Route::get('applicant/examiner', [HomeController::class, 'examiner'])->name('applicant.examiner')->middleware('user_type');
+Route::get('applicant/declined', [HomeController::class, 'declined'])->name('applicant.declined')->middleware('user_type');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('scholar/requirements', 'App\Http\Controllers\scholar@store')->middleware('user_type');
+Route::get('scholar/success', 'App\Http\Controllers\scholar@index')->name('success')->middleware('user_type');
+Route::get('scholar/withfiles', 'App\Http\Controllers\scholar@withfiles')->name('withfiles')->middleware('user_type');
+Route::get('scholar/withoutfiles', 'App\Http\Controllers\scholar@withoutfiles')->name('withoutfiles')->middleware('user_type');
+Route::get('/coordinator/declined', 'App\Http\Controllers\coordinator@index')->name('coordinator.declined')->middleware('user_type');
+Route::get('/coordinator/scholars', 'App\Http\Controllers\coordinator@scholars')->name('coordinator.scholars')->middleware('user_type');
+Route::get('/coordinator/examiners', 'App\Http\Controllers\coordinator@examiners')->name('coordinator.examiners')->middleware('user_type');
+
+Route::post('/coordinator/delete', 'App\Http\Controllers\coordinator@delete')->name('coordinator.delete')->middleware('user_type');
+Route::post('/coordinator/deletes', 'App\Http\Controllers\coordinator@deletes')->name('coordinator.deletes')->middleware('user_type');
+Route::get('/coordinator/accept/{id}', 'App\Http\Controllers\coordinator@accepted')->name('coordinator.accepted')->middleware('user_type');
+Route::get('/coordinator/accepting/{id}', 'App\Http\Controllers\coordinator@accepting')->name('coordinator.accepting')->middleware('user_type');
+Route::get('/coordinator/reject/{id}', 'App\Http\Controllers\coordinator@rejected')->name('coordinator.rejected')->middleware('user_type');
+Route::post('/coordinator/exam/accept/{id}', 'App\Http\Controllers\coordinator@examaccepted')->name('exam.accepted')->middleware('user_type');
+Route::get('/coordinator/exam/reject/{id}', 'App\Http\Controllers\coordinator@examrejected')->name('exam.rejected')->middleware('user_type');
+Route::get('coordinator/applicant', 'App\Http\Controllers\coordinator@applicant')->middleware('user_type');
+
+//register
+Route::get('register/province', [address::class, 'province']);
+Route::get('register/municipality', [address::class, 'municipality']);
+Route::get('register/brgy', [address::class, 'brgy']);
+
+Route::get('/email', function(){
+    Mail::to('johncarlocasipit@gmail.com')->send(new WelcomeMail());
+    return new WelcomeMail();
+});
+
+//welcome
+Route::get('exams/schedule', [coordinator::class, 'examdate']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

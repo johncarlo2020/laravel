79f5d380d30972d409c33dfd84b9cd7458645467
nosetotail-dev <71747675+nosetotail-dev@users.noreply.github.com>
2022-06-22 07:22:29 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -27,42 +27,47 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['App\Http\Middleware\staff']], function() {
-    Route::get('staff/home', [HomeController::class, 'staffHome'])->name('staff.home')->middleware('user_type');
+    Route::get('staff/home', [HomeController::class, 'staffHome'])->name('staff.home');
 });
 
 Route::group(['middleware' => ['App\Http\Middleware\applicant']], function() {
-    Route::get('scholar/success', 'App\Http\Controllers\scholar@index')->name('success')->middleware('user_type');
+    Route::get('scholar/success', 'App\Http\Controllers\scholar@index')->name('success');
+    Route::get('scholar/verify', 'App\Http\Controllers\scholar@verify')->name('verify');
+    Route::get('scholar/withfiles', 'App\Http\Controllers\scholar@withfiles')->name('withfiles');
+    Route::get('applicant/home', [HomeController::class, 'applicantHome'])->name('applicant.home');
+});
+
+Route::group(['middleware' => ['App\Http\Middleware\examiner']], function() {
+    Route::get('scholar/withfiless', 'App\Http\Controllers\scholar@withfiles')->name('withfiless');
 });
 
 Route::group(['middleware' => ['App\Http\Middleware\scholar']], function() {
-    Route::get('applicant/home', [HomeController::class, 'applicantHome'])->name('applicant.home')->middleware('user_type');
-    Route::get('scholar/success', 'App\Http\Controllers\scholar@index')->name('success')->middleware('user_type');
-    Route::post('scholar/requirements', 'App\Http\Controllers\scholar@store')->middleware('user_type');
-    Route::get('scholar/withfiles', 'App\Http\Controllers\scholar@withfiles')->name('withfiles')->middleware('user_type');
-    Route::get('scholar/withoutfiles', 'App\Http\Controllers\scholar@withoutfiles')->name('withoutfiles')->middleware('user_type');
-    Route::get('scholar/scholarhome', 'App\Http\Controllers\scholar@scholarhome')->name('scholarhome')->middleware('user_type');
+    Route::get('scholar/withfilesss', 'App\Http\Controllers\scholar@withfiles')->name('withfilesss');
+    Route::get('scholar/scholarhome', 'App\Http\Controllers\scholar@scholarhome')->name('scholarhome');
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/filez', [HomeController::class, 'filesz'])->name('filesz');
+    Route::post('scholar/requirements', 'App\Http\Controllers\scholar@store');
+
 });
 
 Route::group(['middleware' => ['App\Http\Middleware\coordinator']], function() {
-    Route::get('/coordinator/declined', 'App\Http\Controllers\coordinator@index')->name('coordinator.declined')->middleware('user_type');
-    Route::get('/coordinator/scholars', 'App\Http\Controllers\coordinator@scholars')->name('coordinator.scholars')->middleware('user_type');
-    Route::get('/coordinator/examiners', 'App\Http\Controllers\coordinator@examiners')->name('coordinator.examiners')->middleware('user_type');
-    Route::post('/coordinator/delete', 'App\Http\Controllers\coordinator@delete')->name('coordinator.delete')->middleware('user_type');
-    Route::post('/coordinator/deletes', 'App\Http\Controllers\coordinator@deletes')->name('coordinator.deletes')->middleware('user_type');
-    Route::get('/coordinator/accept/{id}', 'App\Http\Controllers\coordinator@accepted')->name('coordinator.accepted')->middleware('user_type');
-    Route::get('/coordinator/accepting/{id}', 'App\Http\Controllers\coordinator@accepting')->name('coordinator.accepting')->middleware('user_type');
-    Route::get('/coordinator/reject/{id}', 'App\Http\Controllers\coordinator@rejected')->name('coordinator.rejected')->middleware('user_type');
-    Route::post('/coordinator/exam/accept/{id}', 'App\Http\Controllers\coordinator@examaccepted')->name('exam.accepted')->middleware('user_type');
-    Route::get('/coordinator/exam/reject/{id}', 'App\Http\Controllers\coordinator@examrejected')->name('exam.rejected')->middleware('user_type');
-    Route::get('coordinator/applicant', 'App\Http\Controllers\coordinator@applicant')->middleware('user_type');
-    Route::get('coordinator/home', [HomeController::class, 'coordinatorHome'])->name('coordinator.home')->middleware('user_type');
+    Route::get('/coordinator/declined', 'App\Http\Controllers\coordinator@index')->name('coordinator.declined');
+    Route::get('/coordinator/scholars', 'App\Http\Controllers\coordinator@scholars')->name('coordinator.scholars');
+    Route::get('/coordinator/examiners', 'App\Http\Controllers\coordinator@examiners')->name('coordinator.examiners');
+    Route::post('/coordinator/delete', 'App\Http\Controllers\coordinator@delete')->name('coordinator.delete');
+    Route::post('/coordinator/deletes', 'App\Http\Controllers\coordinator@deletes')->name('coordinator.deletes');
+    Route::get('/coordinator/accept/{id}', 'App\Http\Controllers\coordinator@accepted')->name('coordinator.accepted');
+    Route::get('/coordinator/accepting/{id}', 'App\Http\Controllers\coordinator@accepting')->name('coordinator.accepting');
+    Route::get('/coordinator/reject/{id}', 'App\Http\Controllers\coordinator@rejected')->name('coordinator.rejected');
+    Route::post('/coordinator/exam/accept/{id}', 'App\Http\Controllers\coordinator@examaccepted')->name('exam.accepted');
+    Route::get('/coordinator/exam/reject/{id}', 'App\Http\Controllers\coordinator@examrejected')->name('exam.rejected');
+    Route::get('coordinator/applicant', 'App\Http\Controllers\coordinator@applicant');
+    Route::get('coordinator/home', [HomeController::class, 'coordinatorHome'])->name('coordinator.home');
 });
 
 
-Route::get('applicant/examiner', [HomeController::class, 'examiner'])->name('applicant.examiner')->middleware('user_type');
-Route::get('applicant/declined', [HomeController::class, 'declined'])->name('applicant.declined')->middleware('user_type');
+Route::get('applicant/examiner', [HomeController::class, 'examiner'])->name('applicant.examiner');
+Route::get('applicant/declined', [HomeController::class, 'declined'])->name('applicant.declined');
 
 
 //register
